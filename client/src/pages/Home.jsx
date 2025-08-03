e1f9e684e7cd67a0abd70e5a4f1a7f9bec116b50
unknown <eran.hasarali.2001@gmail.com>
2025-08-03 12:46:13 +0530

    add_some_styles

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -23,12 +23,34 @@ const Home = () => {
 
   return (
     <>
-      {users.map((user) => (
-        <div key={user.ID}>
-          <h2>{user.Name}</h2>
-          <p>{user.Email}</p>
-        </div>
-      ))}
+      <table className="min-w-full border border-gray-300 shadow-md rounded-lg overflow-hidden">
+        <thead className="bg-red-500 text-white">
+          <tr>
+            <th className="text-center px-6 py-3 text-sm font-semibold">
+              User ID
+            </th>
+            <th className="text-left px-6 py-3 text-sm font-semibold max-sm:px-1 max-sm:py-2">
+              Name
+            </th>
+            <th className="text-left px-6 py-3 text-sm font-semibold max-sm:px-1 max-sm:py-2">
+              Email
+            </th>
+          </tr>
+        </thead>
+        <tbody className="bg-red-100 divide-y divide-gray-200">
+          {users.map((user) => (
+            <tr key={user.ID}>
+              <td className="px-6 py-4 max-sm:px-1 max-sm:py-2 text-center">
+                {user.ID}
+              </td>
+              <td className="px-6 py-4 max-sm:px-1 max-sm:py-2">{user.Name}</td>
+              <td className="px-6 py-4 max-sm:px-1 max-sm:py-2">
+                {user.Email}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 };
